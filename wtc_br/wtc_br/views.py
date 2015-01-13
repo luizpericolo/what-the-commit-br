18fb0f6ba39ad8f760e5b1d89306830a5abb6e6a
Pericolo <luizpericolo@vialink.com.br>
2015-01-13 19:35:15 -0200

    Views que mostram um comentário aleatório e um comentário específico.

--- wtc_br/wtc_br/views.py
+++ wtc_br/wtc_br/views.py
@@ -1,17 +1,22 @@
 from models import CommitMessage
+from django.shortcuts import render_to_response
 
 def fetch_random_message(request):
-    random_message = CommitMessage.fetch_random_message()
+    message = CommitMessage.fetch_random_message()
 
     ctx = {
-        'permalink': u'{}'.format(message.permalink),
+        'permalink': u'/{}/'.format(message.identifier),
         'message': u'{}'.format(message.text),
     }
+
+    return render_to_response("index.html", ctx)
 
 def fetch_message(request, id):
-    message = CommitMessage.fetch_message(id=id)
+    message = CommitMessage.fetch_message(identifier=id)
 
     ctx = {
-        'permalink': u'{}'.format(message.permalink),
+        'permalink': u'/{}/'.format(message.identifier),
         'message': u'{}'.format(message.text),
     }
+    
+    return render_to_response("index.html", ctx)
